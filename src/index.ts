7140815e2e138e2a3f828c281d4fab497515ce74
Daven Quinn <dev@davenquinn.com>
2021-12-20 02:12:15 -0500

    Split base renderer from Mapbox-specific functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import maplibre, { BasicRenderer } from "maplibre-gl/dist/maplibre-gl-dev";
-import * as Cesium from "cesium";
+import BaseVectorProvider from "./base";
+import maplibre from "maplibre-gl/dist/maplibre-gl-dev";
 
-class VectorProvider {
+class MapboxVectorProvider extends BaseVectorProvider {
   /**
    *
    * @param {Object} options
@@ -15,167 +15,35 @@ class VectorProvider {
    * @param {String} [options.credit] -
    *
    */
-  constructor(options) {
-    console.log(options.style);
-    const transformRequest = (url, type) => {
-      // There has got to be a better way to do this...
-      if (!url.startsWith("mapbox://")) {
-        return { url };
-      }
+  showCanvas: boolean;
 
-      console.log(url, type);
-
-      let prefix = "https://api.mapbox.com/";
-      let suffix = "";
-      if (type === "SpriteJSON" || type === "SpriteImage") {
-        prefix += "styles/v1/";
-        url = url.replace("mapbox://sprites/", prefix);
-        url = url.replace("@2x.png", "/sprite@2x.png");
-        url = url.replace("@2x.json", "/sprite@2x.json");
-      } else if (type === "Style") {
-        url = url.replace("mapbox://styles/", prefix + "styles/v1/");
-      } else if (type == "Source") {
-        prefix += "v4/";
-        url = url.replace("mapbox://", prefix);
-        url += ".json";
-      } else {
-        url = url.replace("mapbox://", prefix);
-      }
-
-      url += "?access_token=" + maplibre.accessToken;
+  transformRequest(url, type) {
+    // A transform request function for Mapbox styles
+    // There has got to be a better way to do this...
+    if (!url.startsWith("mapbox://")) {
       return { url };
-    };
+    }
 
-    this.mapboxRenderer = new BasicRenderer({
-      style: options.style,
-      transformRequest,
-    });
+    let prefix = "https://api.mapbox.com/";
+    let suffix = "";
+    if (type === "SpriteJSON" || type === "SpriteImage") {
+      prefix += "styles/v1/";
+      url = url.replace("mapbox://sprites/", prefix);
+      url = url.replace("@2x.png", "/sprite@2x.png");
+      url = url.replace("@2x.json", "/sprite@2x.json");
+    } else if (type === "Style") {
+      url = url.replace("mapbox://styles/", prefix + "styles/v1/");
+    } else if (type == "Source") {
+      prefix += "v4/";
+      url = url.replace("mapbox://", prefix);
+      url += ".json";
+    } else {
+      url = url.replace("mapbox://", prefix);
+    }
 
-    //this.mapboxRenderer.showCanvasForDebug();
-
-    this.ready = false;
-    this.readyPromise = this.mapboxRenderer._style.loadedPromise.then(() => {
-      this.ready = true;
-    });
-    this.tilingScheme = new Cesium.WebMercatorTilingScheme();
-    this.rectangle = this.tilingScheme.rectangle;
-    this.tileSize = this.tileWidth = this.tileHeight = options.tileSize || 512;
-    this.maximumLevel = options.maximumLevel || Number.MAX_SAFE_INTEGER;
-    this.minimumLevel = options.minimumLevel || 0;
-    this.tileDiscardPolicy = undefined;
-    //this.errorEvent = new Cesium.Event();
-    this.credit = new Cesium.Credit(options.credit || "", false);
-    this.proxy = new Cesium.DefaultProxy("");
-    this.hasAlphaChannel =
-      options.hasAlphaChannel !== undefined ? options.hasAlphaChannel : true;
-    //this.cesiumviewer = options.cesiumViewer;
-    this.sourceFilter = options.sourceFilter;
-    //this.mapboxRenderer.showCanvasForDebug(true);
-  }
-
-  getTileCredits(x, y, level) {
-    return [];
-  }
-
-  createTile() {
-    let canv = document.createElement("canvas");
-    canv.width = this.tileSize;
-    canv.height = this.tileSize;
-    canv.style.imageRendering = "pixelated";
-    canv.getContext("2d").globalCompositeOperation = "copy";
-    return canv;
-  }
-
-  requestImage(x, y, zoom, releaseTile = true) {
-    if (zoom > this.maximumLevel || zoom < this.minimumLevel)
-      return Promise.reject(undefined);
-
-    this.mapboxRenderer.filterForZoom(zoom);
-    const tilesSpec = [];
-    this.mapboxRenderer.getVisibleSources().forEach((s) => {
-      tilesSpec.push({
-        source: s,
-        z: zoom,
-        x: x,
-        y: y,
-        left: 0,
-        top: 0,
-        size: this.tileSize,
-      });
-    });
-
-    console.log(tilesSpec);
-
-    return new Promise((resolve, reject) => {
-      let canv = this.createTile();
-      const renderRef = this.mapboxRenderer.renderTiles(
-        canv.getContext("2d"),
-        {
-          srcLeft: 0,
-          srcTop: 0,
-          width: this.tileSize,
-          height: this.tileSize,
-          destLeft: 0,
-          destTop: 0,
-        },
-        tilesSpec,
-        (err) => {
-          if (!!err) {
-            console.error(err);
-            switch (err) {
-              case "canceled":
-              case "fully-canceled":
-                reject(undefined);
-                break;
-              default:
-                reject(undefined);
-            }
-          } else {
-            if (releaseTile) {
-              renderRef.consumer.ctx = undefined;
-              resolve(canv);
-              // releaseTile默认为true，对应Cesium请求图像的情形
-              this.mapboxRenderer.releaseRender(renderRef);
-            } else {
-              // releaseTile为false时在由pickFeature手动调用，在渲染完成之后在pickFeature里边手动释放tile
-              resolve(renderRef);
-            }
-          }
-        }
-      );
-    });
-  }
-
-  pickFeatures(x, y, zoom, longitude, latitude) {
-    return this.requestImage(x, y, zoom, false).then((renderRef) => {
-      let targetSources = this.mapboxRenderer.getVisibleSources();
-      targetSources = this.sourceFilter
-        ? this.sourceFilter(targetSources)
-        : targetSources;
-
-      const queryResult = [];
-
-      longitude = Cesium.Math.toDegrees(longitude);
-      latitude = Cesium.Math.toDegrees(latitude);
-
-      targetSources.forEach((s) => {
-        queryResult.push({
-          data: this.mapboxRenderer.queryRenderedFeatures({
-            source: s,
-            renderedZoom: zoom,
-            lng: longitude,
-            lat: latitude,
-            tileZ: zoom,
-          }),
-        });
-      });
-
-      // release tile
-      renderRef.consumer.ctx = undefined;
-      this.mapboxRenderer.releaseRender(renderRef);
-      return queryResult;
-    });
+    url += "?access_token=" + maplibre.accessToken;
+    return { url };
   }
 }
 
-export default VectorProvider;
+export default MapboxVectorProvider;
