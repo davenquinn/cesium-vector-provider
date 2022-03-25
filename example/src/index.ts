0bc7518ca2d4ad616139d99e1d04d86d07cbd7a9
Daven Quinn <dev@davenquinn.com>
2022-03-25 14:59:45 -0500

    Add CSS import (this should be handled by cesium-viewer)

--- example/src/index.ts
+++ example/src/index.ts
@@ -2,6 +2,8 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "./main.css";
 import "cesiumSource/Widgets/widgets.css";
+import "@macrostrat/cesium-viewer/dist/index.css";
+import "@znemz/cesium-navigation/dist/index.css";
 import { render } from "react-dom";
 import h from "@macrostrat/hyper";
 import App from "./app";
